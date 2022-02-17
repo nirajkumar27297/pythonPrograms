14d71b8fa2bc5370f91ddb8cd61e16060e26a00f
knoldus <nirajkumar@azuga.com>
2022-02-17 10:05:35 +0530

    Added[nirajkumar17315@gmail.com]:Added python files

---
+++ if-else-block.py
@@ -0,0 +1,12 @@
+
+a = int(input("Enter the number"))
+if a > 0:
+    print("it's a positive number")
+    print("inside if block")
+elif a == 0:
+    print("It's a zero")
+    print("inside elif block")
+    
+print("Code finished")
+
+
